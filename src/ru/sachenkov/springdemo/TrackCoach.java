a7d9108710c47eeb7e2395bfc9ea3d240375556e
Maxim Sachenkov <oqiwhite1996@gmail.com>
2020-04-16 14:39:06 +0300

    SpringCourse

---
+++ src/ru/sachenkov/springdemo/TrackCoach.java
@@ -0,0 +1,20 @@
+package ru.sachenkov.springdemo;
+
+public class TrackCoach implements Coach {
+    private FortuneService fortuneService;
+    public TrackCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    public TrackCoach() {}
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5K";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return "Just do it " + fortuneService.getFortune();
+    }
+}
