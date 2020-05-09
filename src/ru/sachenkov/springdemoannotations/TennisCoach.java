f632e9455d83f6a22ef870eba95ce9c448a1115a
Vallov <oqiwhite1996@gmail.com>
2020-05-09 11:55:26 +0300

    Qualifiers for Dependency Injection

--- src/ru/sachenkov/springdemoannotations/TennisCoach.java
+++ src/ru/sachenkov/springdemoannotations/TennisCoach.java
@@ -1,12 +1,14 @@
 package ru.sachenkov.springdemoannotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
     @Autowired
+    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
     public TennisCoach() {}
